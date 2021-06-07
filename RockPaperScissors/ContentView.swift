4c81c3d7faa02c9a13d5dceddcc5cd3e03be5e2c
daffashiddiq <daffashiddiq@gmail.com>
2021-06-07 08:30:21 +0700

    main

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    var moves = ["Rock","Paper","Scissors"]
+    @State var moveBot = Int.random(in: 0...2)
+    @State var score = false
+    @State var sumScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Test Your Luck!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            Text("Your Score is \(self.sumScore)")
+                .font(.title)
+                .padding()
+            VStack{
+                ForEach(moves, id: \.self){ item in
+                    Button(action: {
+                        self.itemTapped(item)
+                    }){
+                        Text(item)
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.black)
+                            .background(Color.green)
+                    }.padding()
+                }
+            }.alert(isPresented: $score){
+                Alert(title: Text("Alert"), message: Text("Wanna Play Again ?"), dismissButton: .default(Text("Continue")){
+                    self.replay()
+                })
+            }
+        }
+    }
+    
+    func itemTapped(_ item: String) {
+        if(item == "Paper" && moveBot == 0) || (item == "Rock" && moveBot == 2) || (item == "Scissors" && moveBot == 1){
+            sumScore += 1
+        } else if (item == "Rock" && moveBot == 0) || (item == "Paper" && moveBot == 1) || (item == "Scissors" && moveBot == 2){
+            sumScore = sumScore
+        } else {
+            sumScore -= 1
+        }
+        score = true
+    }
+    
+    func replay() {
+        moveBot = Int.random(in: 0...2)
     }
 }
 
